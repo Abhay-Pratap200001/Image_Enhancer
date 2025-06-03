bb9f2507d92f479a7c4c2518edf32ce9b86934cf
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-06-03 10:12:14 +0530

    Basic ui has completed

---
+++ src/component/ImageUpload.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ImageUpload() {
+  return (
+    <div className="bg-white shadow-lg rounded-2xl p-6 max-w-2xl:">
+      <label
+        htmlFor="fileInput"
+        className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-6 text-center
+         hover:border-blue-500 transition-all">
+
+        <input type="file" id="fileInput" className="hidden"/>
+        <span className="text-lg font-medium text-gray-700">Click And Drag To Your Image</span>
+      </label>
+    </div>
+  );
+}
+
+export default ImageUpload;
